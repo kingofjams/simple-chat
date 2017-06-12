7d3940a88e80799de16b53a3ab19a53cc48231a0
chenchang <chenc@pingansec.com>
2017-06-12 19:59:56 +0800

    socket

--- server/worker.py
+++ server/worker.py
@@ -39,37 +39,38 @@ class Worker:
             events = self.epoll.poll(self.time_out)
             if not events:
                 print "epoll超时无活动连接， 重新轮询......"
-            print "有", len(events), "个新事件， 开始处理......"
-            for fd, event in events:
-                _socket = self.fd_to_socket[fd]
-                if _socket == self.server_socket:
-                    connection, address = self.server_socket.accept()
-                    print "新连接:", address
-                    connection.setblocking(False)
-                    self.epoll.register(connection.fileno, select.EPOLLIN)
-                    self.fd_to_socket[connection.fileno] = connection
-                    self.message_queues[connection] = Queue.Queue()
-                elif event & select.EPOLLHUP:
-                    print '客户端已经关闭连接'
-                    # 注销客户句炳
-                    self.epoll.unregister(fd)
-                    self.fd_to_socket[fd].close()
-                    del [fd]
-                elif event & select.EPOLLIN:
-                    data = _socket.recv(1024)
-                    if data:
-                        print "收到数据:", data, "客户端:", _socket.getpeername()
-                        self.message_queues[socket].put(data)
-                        self.epoll.modify(fd, select.EPOLLOUT)
-                elif event & select.EPOLLOUT:
-                    try:
-                        msg = self.message_queues[socket].get_nowait()
-                    except Queue.Empty:
-                        print _socket.getpeername(), "queue empty"
-                        self.epoll.modify(fd, select.EPOLLIN)
-                    else:
-                        print "发送数据：", data, "客户端:", _socket.getpeername()
-                        _socket.send(msg)
+            else:
+                print "有", len(events), "个新事件， 开始处理......"
+                for fd, event in events:
+                    _socket = self.fd_to_socket[fd]
+                    if _socket == self.server_socket:
+                        connection, address = self.server_socket.accept()
+                        print "新连接:", address
+                        connection.setblocking(False)
+                        self.epoll.register(connection.fileno, select.EPOLLIN)
+                        self.fd_to_socket[connection.fileno] = connection
+                        self.message_queues[connection] = Queue.Queue()
+                    elif event & select.EPOLLHUP:
+                        print '客户端已经关闭连接'
+                        # 注销客户句炳
+                        self.epoll.unregister(fd)
+                        self.fd_to_socket[fd].close()
+                        del [fd]
+                    elif event & select.EPOLLIN:
+                        data = _socket.recv(1024)
+                        if data:
+                            print "收到数据:", data, "客户端:", _socket.getpeername()
+                            self.message_queues[socket].put(data)
+                            self.epoll.modify(fd, select.EPOLLOUT)
+                        elif event & select.EPOLLOUT:
+                            try:
+                                msg = self.message_queues[socket].get_nowait()
+                            except Queue.Empty:
+                                print _socket.getpeername(), "queue empty"
+                                self.epoll.modify(fd, select.EPOLLIN)
+                            else:
+                                print "发送数据：", data, "客户端:", _socket.getpeername()
+                                _socket.send(msg)
 
     def close(self):
         self.epoll.unregister(self.server_socket.fileno())
