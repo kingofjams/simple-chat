38edea3bdd462bf52166d394922ccddc30aa6628
chenchang <chenc@pingansec.com>
2017-06-13 19:19:37 +0800

    socket

--- server/worker.py
+++ server/worker.py
@@ -2,36 +2,53 @@
 import socket
 import select
 import Queue
+import os
+import sys
 
 
 class Worker:
     workers = []
+    ip = '0.0.0.0'
+    port = 8090
+    time_out = 10
 
     def __init__(self):
-        self.ip = '0.0.0.0'
-        self.port = 8090
-        self.workers.append('a')
-        self.time_out = 10
+        # 开新的进程
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError, e:
+            sys.stdout.write('fork #1 failed: %d (%s)\n' % (e.errno, e.strerror))
+            sys.exit(1)
+        self.workers.append(pid)
         # 这里epoll只能在linux下才能使用
         self.epoll = select.epoll(self.time_out)
         self.message_queues = {}
         self.fd_to_socket = {}
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except Exception, e:
+            sys.stdout.write('fork #1 failed: (%s)\n' % str(e))
+            sys.exit(1)
         self.create_socket()
         self.ep_read()
 
     def create_socket(self):
-        # SO_REUSEADDR 端口被释放后立即使用
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_address = (self.ip, self.port)
-        self.server_socket.bind(server_address)
-        self.server_socket.listen(10)
-        self.server_socket.setblocking(False)
-        # 注册服务器监听fd到等待读事件集合
-        # EPOLLIN 端口释放后立即启用
-        self.epoll.register(self.server_socket.fileno(), select.EPOLLIN)
-        # 文件句柄到所对应对象的字典，格式为{句柄：对象}
-        self.fd_to_socket = {self.server_socket.fileno(): self.server_socket, }
+        try:
+            # SO_REUSEADDR 端口被释放后立即使用
+            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_address = (self.ip, self.port)
+            self.server_socket.bind(server_address)
+            self.server_socket.listen(10)
+            self.server_socket.setblocking(False)
+            # 注册服务器监听fd到等待读事件集合
+            # EPOLLIN 端口释放后立即启用
+            self.epoll.register(self.server_socket.fileno(), select.EPOLLIN)
+            # 文件句柄到所对应对象的字典，格式为{句柄：对象}
+            self.fd_to_socket = {self.server_socket.fileno(): self.server_socket, }
+        except Exception, e:
+            sys.stdout.write('创建socket失败! %s\n' % str(e))
 
     def ep_read(self):
         while True:
@@ -49,7 +66,7 @@ class Worker:
                         connection.setblocking(False)
                         self.epoll.register(connection.fileno, select.EPOLLIN)
                         self.fd_to_socket[connection.fileno] = connection
-                        self.message_queues[connection] = Queue.Queue()
+                        self.message_queues[_socket] = Queue.Queue()
                     elif event & select.EPOLLHUP:
                         print '客户端已经关闭连接'
                         # 注销客户句炳
@@ -60,7 +77,7 @@ class Worker:
                         data = _socket.recv(1024)
                         if data:
                             print "收到数据:", data, "客户端:", _socket.getpeername()
-                            self.message_queues[socket].put(data)
+                            self.message_queues[_socket].put(data)
                             self.epoll.modify(fd, select.EPOLLOUT)
                         elif event & select.EPOLLOUT:
                             try:
